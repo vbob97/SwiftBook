66f9130f6ac8ff9d816b2f7e5974cae7fa0217ce
vbob97 <vincenzo.bombace@studenti.unicampania.it>
2022-08-03 12:27:48 +0200

    Initial Commit

---
+++ BarberSwiftUI/BarberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberSwiftUIApp.swift
+//  BarberSwiftUI
+//
+//  Created by Vincenzo Bombace on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BarberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
